bc1febcc2b9f069419a47798c7cda11774a83514
Adrian <adguirre160491@outlook.com>
2020-08-03 00:17:15 -0500

    First Release
    First update

---
+++ boxstore.py
@@ -0,0 +1,28 @@
+from scrappingscript import Scrapper
+import pandas as pd
+
+baseurl = "https://thebox.com.pe"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/74.0.3729.169 Safari/537.36 '
+}
+
+web_scrapping = Scrapper(baseurl, headers)
+categories = web_scrapping.get_links("https://thebox.com.pe", "collections", comp='/collections', sw='/')
+
+product_tags = ["grid-view-item grid-view-item--sold-out product-card", "grid-view-item product-card"]
+
+pages_per_cat = web_scrapping.link_pages(categories, product_tags)
+
+products_list = web_scrapping.get_products(pages_per_cat, 'products')
+
+data = web_scrapping.product_data(products_list)
+
+df = pd.DataFrame(data)
+
+df['name'] = df['name'].str.replace("\n", '-')
+df['description'] = df['description'].str.replace("\n", '-')
+#df['color'] = df['color'].str.replace("\n", '-')
+#df['size'] = df['size'].str.replace("\n", '-')
+
+df.to_csv('theboxstore.csv', encoding="utf-8-sig")
---
+++ scrappingscript.py
@@ -0,0 +1,140 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scrapper:
+    """
+    Clase Scrapper
+    """
+    def __init__(self, baseurl, headers):
+        self.baseurl = baseurl
+        self.headers = headers
+
+    # url = url from store
+    # comp = ubication of categories, ex = '/collections'
+    # sw = starts with can be https default, "/" for subdirectories or left it empty
+
+    def get_links(self, url, contains, comp='', sw='https'):
+        """
+        :param url:
+        :param contains:
+        :param comp:
+        :param sw:
+        :return:
+        """
+        r = requests.get(url, headers=self.headers)
+        soup = BeautifulSoup(r.content, 'lxml')
+        category_links = []
+        links = {
+            e.get('href')
+            for e in soup.find_all('a')
+            if e.get('href') and f'{contains}' in e.get('href')
+        }
+        for link in links:
+            if sw == 'https':
+                category_links.append(link)
+            elif sw == '/':
+                category_links.append(self.baseurl + link)
+                # print(re.match(r"(ftp|http|https)://.*\.(pe|com|net)", self.url))
+                # category_links.append(a+_)
+                # print(category_links)
+            else:
+                category_links.append(url + link)
+
+        def fix_links(links_list):
+            """
+
+            :param links_list:
+            :return:
+            """
+            fixed = []
+            for x in links_list:
+                if x.startswith(f'{self.baseurl}{comp}'):
+                    fixed.append(x)
+                else:
+                    continue
+            return fixed
+
+        return fix_links(category_links)
+
+    def link_pages(self, categories, products_tags):
+        """
+
+        :param categories:
+        :param products_tags:
+        :return:
+        """
+        pages_links = []
+        for category in categories:
+            for x in range(1, 200):
+                r = requests.get(f"{category}?page={x}", headers=self.headers)
+                soup = BeautifulSoup(r.content, 'lxml')
+                if soup.find_all('div', {"class": products_tags}):
+                    pages_links.append(f"{category}?page={x}")
+                else:
+                    break
+        return pages_links
+
+    def get_products(self, links, contains):
+        """
+
+        :param links:
+        :param contains:
+        :return:
+        """
+        category_links = []
+        product_links = []
+        for x in links:
+            print(x)
+            r = requests.get(f"{x}", headers=self.headers)
+            soup = BeautifulSoup(r.content, 'lxml')
+            links = [
+                e.get('href') for e in soup.find_all('a')
+                if e.get('href') and contains in e.get('href')
+            ]
+            category_links += links
+        for x in category_links:
+            if x.startswith('https'):
+                product_links.append(x)
+            else:
+                product_links.append(self.baseurl + x)
+        return product_links
+
+    def product_data(self, product_list):
+        """
+
+        :param product_list:
+        :return:
+        """
+        products_info = []
+        count = 0
+        for product in product_list:
+            r = requests.get(product, headers=self.headers)
+            soup = BeautifulSoup(r.content, 'lxml')
+            print(f'Extrayendo data de : {product}')
+            name = soup.find('h1',
+                             class_='product-single__title').string.strip()
+            print(f'Nombre de Producto: {name}')
+            price = soup.find(
+                'span', class_="price-item price-item--sale").text.strip()
+            description = soup.find(
+                'div', class_="product-single__description rte").text.strip()
+            options = soup.find_all(
+                'select',
+                class_=
+                "single-option-selector single-option-selector-product-template product-form__input"
+            )
+
+
+            product = {
+                'name': name,
+                'price': price,
+                'description': description,
+                'options': options,
+                #'size': size,
+                'link': product
+            }
+            count += 1
+            print(count)
+            products_info.append(product)
+        return products_info
